3dffbceb81b66830bfdf0f2c026d455a2f755134
Jhon shihab <jon.shiab1997@gmail.com>
2024-05-15 22:50:21 +0600

    home page loading placeholder

--- src/components/theme/ThemeImage.jsx
+++ src/components/theme/ThemeImage.jsx
@@ -4,30 +4,46 @@ import React, { useState } from "react";
 export default function ThemeImage({ height, src, alt, ...wrapperProps }) {
   const [isLoaded, setIsLoaded] = useState(false);
   return (
-    <div
-      className="image-container"
-      style={{
-        borderRadius: "20px",
-        height: `${height}px`,
-      }}
-      {...{ wrapperProps }}
-    >
-      <Image
-        src={src}
-        // src="https://images.unsplash.com/photo-1715520045597-de56a8639058?q=80&w=686&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt={alt}
-        fill
-        className="image"
-        onLoad={() => setIsLoaded(true)}
-      />
-      {!isLoaded && (
-        <div
-          className="c-skeleton-square"
-          style={{
-            height: `${height}px`,
-          }}
+    <>
+      <style jsx>
+        {`
+          ${isLoaded
+            ? `
+              @media (max-width: 480px) {
+                .image-container {
+                  height: auto !important;
+                }
+              }`
+            : ""}
+        `}
+      </style>
+      <div
+        className="image-container"
+        style={{
+          borderRadius: "20px",
+          height: `${height}px`,
+        }}
+        {...wrapperProps}
+      >
+        <Image
+          src={src}
+          // src="https://images.unsplash.com/photo-1715520045597-de56a8639058?q=80&w=686&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt={alt}
+          fill
+          className="image"
+          onLoad={() => setIsLoaded(true)}
         />
-      )}
-    </div>
+        {!isLoaded && (
+          <div
+            className="skeleton-wrapper"
+            style={{
+              height: `${height}px`,
+            }}
+          >
+            <div className="c-skeleton-square" />
+          </div>
+        )}
+      </div>
+    </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,21 @@
 import React from "react";
+import dynamic from "next/dynamic";
 
 import HomeLayout from "@/components/layouts/home";
 import DownloadAppSection from "@/components/sections/DownloadAppSection";
 import QuickServicesSection from "@/components/sections/QuickServicesSection";
 import AboutSection from "@/components/sections/AboutSection";
 import HomeBannerSection from "@/components/sections/HomeBannerSection";
-import HomeSearchSection from "@/components/sections/HomeSearchSection";
 import HomeBannerRightFroMobileDevice from "@/components/sections/HomeBannerRightFroMobileDevice";
+import HomeSearchSectionLoadingPlaceholder from "@/components/sections/HomeSearchSection/Loading";
+
+const HomeSearchSection = dynamic(
+  () => import("@/components/sections/HomeSearchSection"),
+  {
+    ssr: false,
+    loading: () => <HomeSearchSectionLoadingPlaceholder />,
+  }
+);
 
 export default function HomePage() {
   return (
@@ -15,6 +24,7 @@ export default function HomePage() {
       <HomeSearchSection />
       {/* Banner Right Image For Mobile Device */}
       <HomeBannerRightFroMobileDevice />
+
       {/* Border Starts */}
       <div className="container section_line">
         <div className="row">
