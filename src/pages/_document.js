551923b3e73fc5f5fffa8cc57307b20770bcff35
Jhon shihab <jon.shiab1997@gmail.com>
2024-05-16 09:46:20 +0600

    image skeleton loading fix, meta viewport fixed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, user-scalable=no"
+        />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
       </Head>
       <body>
         <Main />
